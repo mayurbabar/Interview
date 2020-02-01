91d11eae67c757a591c467725e7f25a53e156d05
Babar,Mayur <mayur.babar@cerner.com>
2020-02-01 13:29:50 -0600

    New Microsoft OA problem and fixed critical router driving code

--- Amazon/src/CriticalRouters.java
+++ Amazon/src/CriticalRouters.java
@@ -1,12 +1,12 @@
+// https://leetcode.com/discuss/interview-question/436073/
+
+
 //IMPORT LIBRARY PACKAGES NEEDED BY YOUR PROGRAM
 //SOME CLASSES WITHIN A PACKAGE MAY BE RESTRICTED
 //DEFINE ANY CLASS AND METHOD NEEDED
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
 
 //CLASS BEGINS, THIS CLASS IS REQUIRED
 public class CriticalRouters {
@@ -16,35 +16,58 @@ public class CriticalRouters {
 
 	    int numRouters1 = 7;
 	    int numLinks1 = 7;
-	    List<List<Integer>> links1 = new ArrayList<>();{{0, 1}, {0, 2}, {1, 3}, {2, 3}, {2, 5}, {5, 6}, {3, 4}};
-	    links1.add(Arrays.asList(new int[] {0,1}));
+	    ArrayList<ArrayList<Integer>> links1 = new ArrayList<>();
+	    links1.add(new ArrayList<Integer>() {{add(0); add(1);}});
+	    links1.add(new ArrayList<Integer>() {{add(0); add(2);}});
+	    links1.add(new ArrayList<Integer>() {{add(1); add(3);}});
+	    links1.add(new ArrayList<Integer>() {{add(2); add(3);}});
+	    links1.add(new ArrayList<Integer>() {{add(2); add(5);}});
+	    links1.add(new ArrayList<Integer>() {{add(5); add(6);}});
+	    links1.add(new ArrayList<Integer>() {{add(3); add(4);}});
 
-	    List<Integer> res = obj.criticalRouters(numRouters1, numLinks1, Arrays.asList(links1));
+
+	    List<Integer> res = obj.criticalRouters(numRouters1, numLinks1, links1);
 
 	    for(int i: res) System.out.print(i + " ");
 	    System.out.println();
 
 	    int numRouters2 = 5;
 	    int numLinks2 = 5;
-	    int[][] links2 = {{1,2}, {0,1}, {2,0}, {0,3}, {3,4}};
+	    ArrayList<ArrayList<Integer>> links2 = new ArrayList<>();
+	    links2.add(new ArrayList<Integer>() {{add(1); add(2);}});
+	    links2.add(new ArrayList<Integer>() {{add(0); add(1);}});
+	    links2.add(new ArrayList<Integer>() {{add(2); add(0);}});
+	    links2.add(new ArrayList<Integer>() {{add(0); add(3);}});
+	    links2.add(new ArrayList<Integer>() {{add(3); add(4);}});
 
-	    List<Integer> res2 = obj.getCriticalRouters(numRouters2, numLinks2, links2);
+	    List<Integer> res2 = obj.criticalRouters(numRouters2, numLinks2, links2);
 	    for(int i: res2) System.out.print(i + " ");
 	    System.out.println();
 
 	    int numRouters3 = 4;
 	    int numLinks3 = 4;
-	    int[][] links3 = {{0,1},{1,2},{2,3}};
+	    ArrayList<ArrayList<Integer>> links3 = new ArrayList<>();
+	    links3.add(new ArrayList<Integer>() {{add(0); add(1);}});
+	    links3.add(new ArrayList<Integer>() {{add(1); add(2);}});
+	    links3.add(new ArrayList<Integer>() {{add(+2); add(3);}});
 
-	    List<Integer> res3 = obj.getCriticalRouters(numRouters3, numLinks3, links3);
+	    List<Integer> res3 = obj.criticalRouters(numRouters3, numLinks3, links3);
 	    for(int i: res3) System.out.print(i + " ");
 	    System.out.println();
 
 	    int numRouters4 = 7;
 	    int numLinks4 = 8;
-	    int[][] links4 = {{0,1},{0,2},{1,2},{1,3},{1,4},{1,6},{3,5},{4,5}};
+	    ArrayList<ArrayList<Integer>> links4 = new ArrayList<>();
+	    links4.add(new ArrayList<Integer>() {{add(0); add(1);}});
+	    links4.add(new ArrayList<Integer>() {{add(0); add(2);}});
+	    links4.add(new ArrayList<Integer>() {{add(1); add(2);}});
+	    links4.add(new ArrayList<Integer>() {{add(1); add(3);}});
+	    links4.add(new ArrayList<Integer>() {{add(1); add(4);}});
+	    links4.add(new ArrayList<Integer>() {{add(1); add(6);}});
+	    links4.add(new ArrayList<Integer>() {{add(3); add(5);}});
+	    links4.add(new ArrayList<Integer>() {{add(4); add(5);}});
 
-	    List<Integer> res4 = obj.getCriticalRouters(numRouters4, numLinks4, links4);
+	    List<Integer> res4 = obj.criticalRouters(numRouters4, numLinks4, links4);
 	    for(int i: res4) System.out.print(i + " ");
 	    System.out.println();
 	}
@@ -100,8 +123,10 @@ public class CriticalRouters {
 			int u = list.get(0);
 			int v = list.get(1);
 
-			adjList.get(u - 1).add(v - 1);
-			adjList.get(v - 1).add(u - 1);
+//			adjList.get(u - 1).add(v - 1);
+//			adjList.get(v - 1).add(u - 1);
+			adjList.get(u).add(v);
+			adjList.get(v).add(u);
 		}
 	}
 
---
+++ Microsoft/src/PlaneSeatReservation.java
@@ -0,0 +1,58 @@
+// https://leetcode.com/discuss/interview-question/492652/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlaneSeatReservation {
+
+	public static void main(String[] args) {
+		System.out.println(solution(2, "1A 2F 1C"));          // 2
+		System.out.println(solution(3, "1A 2F 1C 3A 3K"));    // 4
+		System.out.println(solution(3, "1A 2F 1C 3C 3J"));    // 3
+		System.out.println(solution(0, "1A 2F 1C 3C 3J"));    // 0
+		System.out.println(solution(3, null));                // 0
+		System.out.println(solution(3, ""));                  // 6
+		System.out.println(solution(2, "1J 2F 2B 2H 1E 1C")); // 0
+		
+	}
+	
+	public static int solution(int N, String S) {
+		int family =0;
+		Set<String> bookedSeats = new HashSet<>();
+		
+		// if plane have 0 rows then we can't assign any seat.
+		// Assuming S is never null. If it is null then it's a bad data so return 0.
+		// if S is empty that means none of the seat is booked.
+		if(N <=0 || S == null)
+			return 0;
+		
+		String[] arr = S.split(" ");
+		
+		for(String str : arr)
+			bookedSeats.add(str);
+		
+		for(int i=1; i<=N; i++) {
+			if(!bookedSeats.contains(i+"B") && !bookedSeats.contains(i+"C") && !bookedSeats.contains(i+"D") && !bookedSeats.contains(i+"E")) {
+				family++;
+				bookedSeats.add(i+"B");
+				bookedSeats.add(i+"C");
+				bookedSeats.add(i+"D");
+				bookedSeats.add(i+"E");
+			}
+			
+			if(!bookedSeats.contains(i+"F") && !bookedSeats.contains(i+"G") && !bookedSeats.contains(i+"H") && !bookedSeats.contains(i+"J")) {
+				family++;
+				bookedSeats.add(i+"F");
+				bookedSeats.add(i+"G");
+				bookedSeats.add(i+"H");
+				bookedSeats.add(i+"J");
+			}
+			
+			if(!bookedSeats.contains(i+"D") && !bookedSeats.contains(i+"E") && !bookedSeats.contains(i+"F") && !bookedSeats.contains(i+"G"))
+				family++;
+		}
+		
+		return family;
+	}
+
+}
